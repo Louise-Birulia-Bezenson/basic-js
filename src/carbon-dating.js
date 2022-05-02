84bde8c715288b9bdef919b0cddebb2b81c52d89
Louise-Birulia-Bezenson <elizabethbirulia@gmail.com>
2022-05-03 02:31:48 +0300

    feat: 1 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ 
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string" || /[A-Za-z]/.test(sampleActivity) || sampleActivity >=15 || sampleActivity <= 0){
+return false;
+  }
+
+ 
+  if (/[0-9]/.test(sampleActivity)){
+   let k = 0.693 /HALF_LIFE_PERIOD;
+     let t = Math.log(MODERN_ACTIVITY / +sampleActivity)/k;
+    
+     return Math.ceil(t);
+   } else{
+     return false;
+   }
+ 
 }
 
 module.exports = {
